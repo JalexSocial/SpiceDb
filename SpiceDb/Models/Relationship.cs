a607215d877edf9321b5789a9b6151cd4b7cce73
Michael Tanczos <tanczosm@eastonsd.org>
2023-02-16 13:30:54 -0500

    Reverting conversion

--- SpiceDb/Models/Relationship.cs
+++ SpiceDb/Models/Relationship.cs
@@ -58,13 +58,6 @@ public class Relationship
 
     public override string ToString()
     {
-        var relationship = $"{Resource.ToString()}#{this.Relation}@{Subject.ToString()}";
-
-        if (!string.IsNullOrEmpty(this.Subject.Relation))
-        {
-            relationship += $"#{this.Subject.Relation}";
-        }
-
-        return relationship;
+        return $"{Resource.ToString()}#{this.Relation}@{Subject.ToString()}";
     }
 }

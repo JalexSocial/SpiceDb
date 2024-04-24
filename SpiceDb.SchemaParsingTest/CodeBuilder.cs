fc18a265ccdaf8d1de5c99aa7bc20dffa6b209c3
Michael Tanczos <tanczosm@eastonsd.org>
2024-04-24 12:12:14 -0400

    refactor: Improve code structure in SchemaParsingTest
    Significant changes have been made to the CodeBuilder class in the SchemaParsingTest. The logic for handling relationships and permissions has been refactored for better readability and maintainability.
    
    - Introduced a new Dictionary to store relationship definitions, which simplifies the process of adding optional relations.
    - Extracted complex permission resolution logic into a separate method, ResolvePermissionSubjects, improving code clarity.
    - Created an internal class RelationshipCodeDefinition to encapsulate related properties, enhancing code organization.
    
    No changes were made to production code functionality.

--- SpiceDb.SchemaParsingTest/CodeBuilder.cs
+++ SpiceDb.SchemaParsingTest/CodeBuilder.cs
@@ -28,6 +28,8 @@ public class CodeBuilder
 
 		foreach (var definition in _parser.Schema.Definitions)
 		{
+			Dictionary<string, RelationshipCodeDefinition> codeDef = new();
+
 			foreach (var relation in definition.Relations)
 			{
 				foreach (var relType in relation.Types)
@@ -39,15 +41,40 @@ public class CodeBuilder
 					//	Console.Write($" (with subject relation {relType.Relation})");
 
 					//Console.WriteLine();
+					var relationshipDef = new RelationshipCodeDefinition
+					{
+						MethodName = $"Relate{relation.Name.Pascalize()}",
+						SubjectName = $"Zed{relType.Type.Pascalize()}",
+						Relation = relation.Name
+					};
+
+					var key = $"{relationshipDef.MethodName}:{relationshipDef.SubjectName}";
+					codeDef.TryAdd(key, relationshipDef);
 
 					if (!string.IsNullOrEmpty(relType.Relation))
 					{
-						definitionCode[definition.Name].AppendLine($"    public Relationship Relate{relation.Name.Pascalize()}(Zed{relType.Type.Pascalize()} subject) => new Relationship(this, \"{relation.Name}\", subject.WithSubjectRelation(\"{relType.Relation}\"));");
+						codeDef[key].OptionalRelations.Add(relType.Relation);
 					}
-					else
+
+					// How to add to codeDef?
+				}
+			}
+
+			foreach (var key in codeDef.Keys)
+			{
+				var relationshipDef = codeDef[key];
+
+				if (relationshipDef.OptionalRelations.Count > 0)
+				{
+					foreach (var optionalRelation in relationshipDef.OptionalRelations)
 					{
-						definitionCode[definition.Name].AppendLine($"    public Relationship Relate{relation.Name.Pascalize()}(Zed{relType.Type.Pascalize()} subject) => new Relationship(this, \"{relation.Name}\", subject);");
+						var relText = optionalRelation == "*" ? "All" : optionalRelation.Pascalize();
+						definitionCode[definition.Name].AppendLine($"    public Relationship {relationshipDef.MethodName}{relText}({relationshipDef.SubjectName} subject) => new Relationship(this, \"{relationshipDef.Relation}\", subject.WithSubjectRelation(\"{optionalRelation}\"));");
 					}
+				}
+				else
+				{
+					definitionCode[definition.Name].AppendLine($"    public Relationship {relationshipDef.MethodName}({relationshipDef.SubjectName} subject) => new Relationship(this, \"{relationshipDef.Relation}\", subject);");
 				}
 			}
 
@@ -58,40 +85,12 @@ public class CodeBuilder
 				var permission = permissions[k];
 				var key = !permission.Contains("#") ? $"{definition.Name}#{permission}" : permission;
 
-				if (_parser.PermissionMap.ContainsKey(key))
+				//  public SpiceDb.Models.Permission OrgIsAdministrator(ZedOrganization resource) => new SpiceDb.Models.Permission(resource, "admin", this);
+				var subjects = ResolvePermissionSubjects(key);
+
+				foreach (var subject in subjects)
 				{
-					//  public SpiceDb.Models.Permission OrgIsAdministrator(ZedOrganization resource) => new SpiceDb.Models.Permission(resource, "admin", this);
-					List<string> pmap = new(_parser.PermissionMap[key]);
-					HashSet<string> subjects = new();
-
-					for (int i = 0; i < pmap.Count; i++)
-					{
-						var relation = pmap[i];
-
-						// relation is either a true relation or another permission
-						if (_parser.RelationshipMap.ContainsKey(relation))
-						{
-							foreach (var prel in _parser.RelationshipMap[relation])
-								subjects.Add(prel);
-						}
-						else
-						{
-							if (relation.Contains("->")) // Skip synthetic permissions for a while
-							{
-								var prel = relation.Replace("->", "#");
-								if (!permissions.Contains(prel))
-									permissions.Add(prel);
-								continue;
-							}
-
-							pmap.AddRange(new List<string>(_parser.PermissionMap[relation]).Except(pmap));
-						}
-					}
-
-					foreach (var subject in subjects)
-					{
-						definitionCode[subject].AppendLine($"    public SpiceDb.Models.Permission {definition.Name.Pascalize()}{permission.Pascalize()}(Zed{definition.Name.Pascalize()} resource) => new SpiceDb.Models.Permission(resource, \"{permission}\", this);");
-					}
+					definitionCode[subject].AppendLine($"    public SpiceDb.Models.Permission {definition.Name.Pascalize()}{permission.Pascalize()}(Zed{definition.Name.Pascalize()} resource) => new SpiceDb.Models.Permission(resource, \"{permission}\", this);");
 				}
 
 			}
@@ -106,4 +105,59 @@ public class CodeBuilder
 
 		return relationshipCode.ToString();
 	}
+
+	/// <summary>
+	/// Format of permission is definition#permission_name
+	/// </summary>
+	/// <param name="permission"></param>
+	/// <returns></returns>
+	private HashSet<string> ResolvePermissionSubjects(string permission)
+	{
+		HashSet<string> subjects = new();
+
+		if (_parser.PermissionMap.ContainsKey(permission))
+		{
+			List<string> pmap = new(_parser.PermissionMap[permission]);
+
+			for (int i = 0; i < pmap.Count; i++)
+			{
+				var relation = pmap[i];
+
+				// relation is either a true relation or another permission
+				if (_parser.RelationshipMap.ContainsKey(relation))
+				{
+					foreach (var prel in _parser.RelationshipMap[relation])
+						subjects.Add(prel);
+				}
+				else
+				{
+					if (relation.Contains("->")) // Skip synthetic permissions for a while
+					{
+						var prel = relation.Replace("->", "#");
+
+						var otherSubjects = ResolvePermissionSubjects(prel);
+
+						foreach (var otherSubject in otherSubjects)
+						{
+							subjects.Add(otherSubject);
+						}
+
+						continue;
+					}
+
+					pmap.AddRange(new List<string>(_parser.PermissionMap[relation]).Except(pmap));
+				}
+			}
+		}
+
+		return subjects;
+	}
+}
+
+internal class RelationshipCodeDefinition
+{
+	public string MethodName { get; set; } = string.Empty;
+	public string SubjectName { get; set; } = string.Empty;
+	public string Relation { get; set; } = string.Empty;
+	public List<string> OptionalRelations { get; set; } = new();
 }
--- SpiceDb.SchemaParsingTest/Schema.cs
+++ SpiceDb.SchemaParsingTest/Schema.cs
@@ -6,6 +6,7 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SpiceDb.SchemaParsingTest;
+
 public class Schema
 {
 	[JsonPropertyName("definitions")] 
